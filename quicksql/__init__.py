d47bbaf0e3667391784c85e8be5a4222946ab430
Charles Marks <charlesmarksco@gmail.com>
2026-01-02 12:48:07 -0600

    Stubs inclusion into wheel, and format standard.

--- quicksql/__init__.py
+++ quicksql/__init__.py
@@ -1,7 +1,8 @@
 """Parses queries from an sql file, and turns them into callable f-strings. Also a file cache decorator for slow queries and multi-session permanence, supports async functions."""
 
-__version__ = '1.0.2.2'
+__version__ = '1.0.3'
 
-from ._quicksql import file_cache,test_cache,Query,LoadSQL,clear_cache,NoStr
+
+from ._quicksql import LoadSQL, NoStr, Query, clear_cache, file_cache, test_cache
 
 __all__ = ["file_cache", "test_cache", "Query", "LoadSQL", "clear_cache", "NoStr"]
--- quicksql/__init__.pyi
+++ quicksql/__init__.pyi
@@ -1,58 +1,26 @@
 
-
-
-def file_cache(use_mem_cache=..., threadsafe=...): # -> Callable[..., Callable[..., CoroutineType[Any, Any, Any | _NA]] | Callable[..., Any]]:
-    
-    ...
-
-def clear_cache(clr_mem=..., clr_file=...):# -> None:
-    ...
-
-@file_cache()
-def test_cache(*args, **kwargs): # -> tuple[tuple[Any, ...], dict[str, Any]]:
-    ...
-
-class NoStr:
-    __slots__ = ...
-    def __init__(self, string: str) -> None:
-        ...
-    
-    def __str__(self) -> str:
-        ...
-    
-    def __repr__(self): # -> str:
-        ...
-    
-
-
-class Query:
-    
-    def __init__(self, name, query) -> None:
-        
-        ...
-    
-    def __call__(self, **kwargs): # -> Any:
-        ...
-    
-    @cache
-    def __str__(self) -> str:
-        ...
-    
-
-
-class LoadSQL:
-    
-    def __init__(self, path) -> None:
-        
-        ...
-    
-    @cache
-    def __str__(self) -> str:
-        ...
-    
-    @cache
-    def __repr__(self): # -> str:
-        ...
+# from functools import cache
+# 
+# class _NA: ...
+# 
+# def file_cache(use_mem_cache=True, threadsafe=True): ...
+# def clear_cache(clr_mem=True, clr_file=True): ...
+# @file_cache()
+# def test_cache(*args, **kwargs): ...
+# 
+# class NoStr:
+#     __slots__ = ...
+#     def __init__(self, string: str) -> None: ...
+#     def __repr__(self): ...
+# 
+# class Query:
+#     def __init__(self, name, query) -> None: ...
+#     def __call__(self, **kwargs): ...
+# 
+# class LoadSQL:
+#     def __init__(self, path) -> None: ...
+#     @cache
+#     def __repr__(self): ...
 
 
 from ._quicksql import LoadSQL, NoStr, Query, clear_cache, file_cache, test_cache
